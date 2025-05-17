223c37e9a28259bc620787d24844c2e36faf72c0
Minh Hùng <hug2k4123@gmail.com>
2025-05-17 15:19:47 +0700

    test lan 2

--- src/main/java/dev/backend/webbanthucung/Test.java
+++ src/main/java/dev/backend/webbanthucung/Test.java
@@ -7,14 +7,11 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-
-
-       Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {Scanner sc = new Scanner(System.in);
        NguoiDung nd = new NguoiDung();
        nd.nhap(sc);
 
-        DangNhapVNUA dn = new DangNhapVNUA(nd);
+       DangNhapVNUA dn = new DangNhapVNUA(nd);
        dn.dangNhap();
        dn.layDuLieuLichHoc();
     }
